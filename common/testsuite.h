30b3781c8bcf6010d91a1a6a97ebec38537f5fc2
cliff.wong <cliff.wong@modalai.com>
2021-06-16 09:47:54 -0700

    apriltag-as-a-sensor message added to support precision land

--- common/testsuite.h
+++ common/testsuite.h
@@ -4540,6 +4540,69 @@ static void mavlink_test_vehicle_gps_position(uint8_t system_id, uint8_t compone
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_vision_marker_estimate(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_VISION_MARKER_ESTIMATE >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_vision_marker_estimate_t packet_in = {
+        93372036854775807ULL,963497880,101.0,129.0,157.0,185.0,{ 213.0, 214.0, 215.0, 216.0, 217.0, 218.0, 219.0, 220.0, 221.0 },465.0,20771,20875
+    };
+    mavlink_vision_marker_estimate_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.usec = packet_in.usec;
+        packet1.id = packet_in.id;
+        packet1.size_m = packet_in.size_m;
+        packet1.tx = packet_in.tx;
+        packet1.ty = packet_in.ty;
+        packet1.tz = packet_in.tz;
+        packet1.float_reserved3 = packet_in.float_reserved3;
+        packet1.int_reserved1 = packet_in.int_reserved1;
+        packet1.int_reserved2 = packet_in.int_reserved2;
+        
+        mav_array_memcpy(packet1.R, packet_in.R, sizeof(float)*9);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_VISION_MARKER_ESTIMATE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_VISION_MARKER_ESTIMATE_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_vision_marker_estimate_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_vision_marker_estimate_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_vision_marker_estimate_pack(system_id, component_id, &msg , packet1.usec , packet1.id , packet1.size_m , packet1.tx , packet1.ty , packet1.tz , packet1.R , packet1.int_reserved1 , packet1.int_reserved2 , packet1.float_reserved3 );
+    mavlink_msg_vision_marker_estimate_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_vision_marker_estimate_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.usec , packet1.id , packet1.size_m , packet1.tx , packet1.ty , packet1.tz , packet1.R , packet1.int_reserved1 , packet1.int_reserved2 , packet1.float_reserved3 );
+    mavlink_msg_vision_marker_estimate_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_vision_marker_estimate_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_vision_marker_estimate_send(MAVLINK_COMM_1 , packet1.usec , packet1.id , packet1.size_m , packet1.tx , packet1.ty , packet1.tz , packet1.R , packet1.int_reserved1 , packet1.int_reserved2 , packet1.float_reserved3 );
+    mavlink_msg_vision_marker_estimate_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_optical_flow(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -13321,6 +13384,7 @@ static void mavlink_test_common(uint8_t system_id, uint8_t component_id, mavlink
     mavlink_test_hil_actuator_controls(system_id, component_id, last_msg);
     mavlink_test_gps_metadata(system_id, component_id, last_msg);
     mavlink_test_vehicle_gps_position(system_id, component_id, last_msg);
+    mavlink_test_vision_marker_estimate(system_id, component_id, last_msg);
     mavlink_test_optical_flow(system_id, component_id, last_msg);
     mavlink_test_global_vision_position_estimate(system_id, component_id, last_msg);
     mavlink_test_vision_position_estimate(system_id, component_id, last_msg);
